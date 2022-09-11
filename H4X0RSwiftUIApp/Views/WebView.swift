76d6056e147c07964638e9183e6a47e32696d74a
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-11 22:04:54 +0530

    Added views to have navigation from one screen to another and show web views when a list value is tapped on home screen

--- H4X0RSwiftUIApp/Views/WebView.swift
+++ H4X0RSwiftUIApp/Views/WebView.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import WebKit
+import SwiftUI
+
+// below struct helps in having SwiftUIView that represents UIKitView
+struct WebView: UIViewRepresentable{
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WebView.UIViewType {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeUrl = urlString{
+            if let url = URL(string: safeUrl){
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
