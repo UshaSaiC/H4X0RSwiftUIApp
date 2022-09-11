4af8bf687631711627b017829a2efa96cf982119
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-11 20:41:47 +0530

    Initial Commit

---
+++ H4X0RSwiftUIApp/H4X0RSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RSwiftUIAppApp.swift
+//  H4X0RSwiftUIApp
+//
+//  Created by Usha Sai Chintha on 11/09/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
